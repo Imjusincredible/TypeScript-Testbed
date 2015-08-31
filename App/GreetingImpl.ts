5e1bfc693f474af6abc2f70e21086f75f0f527c3
Charles Johnson <git@jfamstudios.com>
2015-08-31 16:52:04 -0700

    NO-*: chore(app): Cleanup Copyrights

--- App/GreetingImpl.ts
+++ App/GreetingImpl.ts
@@ -1,7 +1,7 @@
 /* Copyright (C) ProMedia Development Systems, Inc - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
- * Written by Charles Johnson, II <charlesjohnson@jfamstudios.com>, August 2015
+ * Written by Charles Johnson, II - August 2015
  */
 
 import Person = require("./PersonImpl");
